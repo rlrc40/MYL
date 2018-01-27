6775104a794345e8c023ac189cec7a60dc53ca96
Raúl Rodríguez <raul.rodriguez.c@outlook.com>
2018-01-27 22:31:32 +0100

    [MOD] Controller of users

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -15,59 +15,24 @@ router.delete('/:_id', _delete);
 
 // POST
 function register(req, res) {
-    console.log('Controller register: ', req.body);
     userService.create(req, res);
-        // .then(function () {
-        //     res.sendStatus(200);
-        // })
-        // .catch(function (err) {
-        //     res.status(400).send(err);
-        // });
 }
 
 // GET
 function getAll(req, res) {
-    userService.getAll()
-        .then(function (users) {
-            res.send(users);
-        })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
+    userService.getAll(req, res);
 }
 
 function getById(req, res) {
-    userService.getById(req.params._id)
-        .then(function (user) {
-            if (user) {
-                res.send(user);
-            } else {
-                res.sendStatus(404);
-            }
-        })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
+    userService.getById(req, res);
 }
 
 // PUT
 function update(req, res) {
-    userService.update(req.params._id, req.body)
-        .then(function () {
-            res.sendStatus(200);
-        })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
+    userService.update(req, res);
 }
 
 // DELETE
 function _delete(req, res) {
-    userService.delete(req.params._id)
-        .then(function () {
-            res.sendStatus(200);
-        })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
+    userService._delete(req, res);
 }
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const express = require('express');     
+const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 
 const port = process.env.NODE_ENV === 'production' ? 80 : 3000;
-const url = "mongodb://localhost/talketin";
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// routes
+// controllers
 app.use('/users', require('./controllers/users-controller'));
 
 
