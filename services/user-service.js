b99fe726f096c90eee68c3d108ed343d1e888838
Raúl Rodríguez <raul.rodriguez.c@outlook.com>
2018-01-29 20:41:44 +0100

    [MOD] CREATE User

--- services/user-service.js
+++ services/user-service.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const Group = require('../models/Group')
 const mongoose = require('mongoose')
 const urlDB = "mongodb://localhost/myl"
 
@@ -23,9 +24,9 @@ module.exports = service
 function getAll(req, res) {
     User.find(
       {}, (err, users) => {
-            if (err) res.send(500, err.name + ': ' + err.message)
-            if (!users) res.status(404, {message: "Not found users"})
-            res.status(200, {users})
+            if (err) res.send(500).send(err.name + ': ' + err.message)
+            if (!users) res.status(404).send({message: "Not found users"})
+            res.status(200).send({users})
     })
 }
 
@@ -33,9 +34,9 @@ function getById(req, res) {
     let userId = req.params.userId
     User.findById(
         userId, (err, user) => {
-            if (err) res.send(500, err.name + ': ' + err.message)
-            if (!user) res.status(404 ,{message: "User not found"})
-            else res.status(200, {user})
+            if (err) res.send(500).send(err.name + ': ' + err.message)
+            if (!user) res.status(404).send({message: "User not found"})
+            else res.status(200).send({user})
     })
 }
 
@@ -48,11 +49,37 @@ function create(req, res) {
     user.description = userParam.description
     user.age = userParam.age
     user.created_at = Date.now()
+    user.gender = userParam.gender
+  	user.occupation = userParam.occupation
+  	user.password = userParam.password
+  	user.nativeLanguage = userParam.nativeLanguage
+  	user.languagesToLearn = [userParam.languagesToLearn]
+  	user.facebookAddress = userParam.facebookAddress
+  	user.facebookAvatar = userParam.facebookAvatar
+  	user.facebookLikedPages = userParam.facebookLikedPages
+  	user.facebookGroups = userParam.facebookGroups
+  	user.facebookMusic = userParam.facebookMusic
+  	user.facebookFilm = userParam.facebookFilm
+  	user.facebookBooks = userParam.facebookBooks
+  	user.facebookSeries = userParam.facebookSeries
+  	user.facebookSports = userParam.facebookSports
+  	user.facebookFriends = userParam.facebookFriends
+  	user.instagramAccount = userParam.instagramAccount
+  	user.twitterAccount = userParam.twitterAccount
+  	user.skype = userParam.skype
+  	user.notifications = []
+    user.messages = 0
+  	user.contadorWithoutMessages = 0
+  	user.contadorMessages = 0
+  	user.level = 0
+  	user.languageLevel = 0
+  	user.groups = []
+  	user.receivedLikes = 0
+  	user.givenLikes = 0
 
     user.save((err, userStored) => {
-        if (err) res.status(500, {message: 'Failed at store user in the database'})
-
-        res.status(200, {message: 'User ' + userParam.name + ' has been created'})
+        if (err) res.status(500).send({message: 'Failed at store user in the database'})
+        else res.status(200).send({message: 'User ' + userParam.name + ' has been created', user: userStored})
     })
 }
 
@@ -62,10 +89,10 @@ function validateUser(req, res) {
 
     User.findOne(
         { email: email }, (err, user) => {
-            if (err) res.send(400, err.name + ': ' + err.message)
+            if (err) res.send(400).send(err.name + ': ' + err.message)
 
             if (user) {
-                res.send(412, {message: 'Email ' + email + ' is already taken'})
+                res.send(412).send({message: 'Email ' + email + ' is already taken'})
             } else {
                 create(req, res)
             }
@@ -78,9 +105,9 @@ function update(req, res) {
   let update = req.body
 
   User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
-    if (err || !userUpdated) res.send(500, 'Error at update user: ' + err.message)
+    if (err || !userUpdated) res.send(500).send('Error at update user: ' + err.message)
 
-    res.status(200, {message: 'User ' + userUpdated.name + ' has been updated'})
+    res.status(200).send({message: 'User ' + userUpdated.name + ' has been updated'})
   })
 }
 
@@ -88,10 +115,10 @@ function _delete(req, res) {
   let userId = req.params.userId
 
   User.findById(userId, (err, user) => {
-    if (err || !user) res.status(404, {message: "Error at deleting user: " + userId + " not found"})
+    if (err || !user) res.status(404).send({message: "Error at deleting user: " + userId + " not found"})
     user.remove(err => {
-        if (err) res.status(500, {message: "Error at deleting user: " + userId})
-        res.status(200, {message: "User has been deleted"})
+        if (err) res.status(500).send({message: "Error at deleting user: " + userId})
+        res.status(200).send({message: "User has been deleted"})
     })
   })
 }
