5f7b40e9fd37f1ec9a8dd6d5c7b380161216b651
Raúl Rodríguez <raul.rodriguez.c@outlook.com>
2018-02-26 22:48:17 +0100

    [ADD] Notification Service

---
+++ services/notification-service.js
@@ -0,0 +1,90 @@
+const Notification = require('../models/Notification')
+
+var service = {}
+service.getNotificationsByUserId = getNotificationsByUserId
+service.create = create
+service.update = update
+service._delete = _delete
+
+module.exports = service
+
+
+
+function getNotificationsByUserId(req, res) {
+  let userId = req.params.userId
+  Notification.find({
+    user: userId
+  }, (err, notifications) => {
+    if (err) return res.status(500).send(err.name + ': ' + err.message)
+    if (!notifications) return res.status(404).send({
+      notification: "Not found notifications"
+    })
+    res.status(200).send(notifications)
+  })
+}
+
+function create(req, res) {
+  let notificationParam = req.body
+  let notification = new Notification()
+
+  notification.user = notificationParam.user
+  notification.type = notificationParam.type
+  notification.url = notificationParam.url
+  notification.text = notificationParam.text
+  notification.visited = false
+  notification.created_at = Date.now()
+
+
+  notification.save((err, notificationSent) => {
+    if (err) return res.status(500).send({
+      information: 'Error when sending the notification to the database'
+    })
+    res.status(200).send({
+      information: 'Notification has been sent',
+      notification: notificationSent
+    })
+  })
+}
+
+function update(req, res) {
+  let notificationId = req.params.notificationId
+  let update = req.body
+
+  Notification.findByIdAndUpdate(notificationId, update, {
+    new: true
+  }, (err, notificationUpdated) => {
+    if (err) return res.status(500).send({
+      information: 'Error at update notification: ' + err.message
+    })
+    if (!notificationUpdated) return res.status(404).send({
+      information: 'Notification not found'
+    })
+    res.status(200).send({
+      information: 'Notification has been updated',
+      notification: notificationUpdated
+    })
+  })
+}
+
+function _delete(req, res) {
+  let notificationId = req.params.notificationId
+
+  Notification.findById(notificationId, (err, notification) => {
+    if (err) return res.status(500).send({
+      information: 'Error at update notification: ' + err.message
+    })
+    if (!notification) return res.status(404).send({
+      information: 'Notification not found'
+    })
+
+    notification.remove(err => {
+      if (err) return res.status(500).send({
+        information: "Error at deleting notification: " + notificationId
+      })
+
+      res.status(200).send({
+        information: "Notification has been deleted"
+      })
+    })
+  })
+}
---
+++ test/notification-test.js
@@ -0,0 +1,151 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test'
+
+let Notification = require('../models/Notification')
+
+//Require the dev-dependencies
+let chai = require('chai')
+let chaiHttp = require('chai-http')
+let server = require('../index')
+let should = chai.should()
+
+const getNotification = (type, text) => new Notification({
+  user: "5a78800f2b225224f01d167a",
+  type: type,
+  url: "www.url.com",
+  text: text
+})
+
+chai.use(chaiHttp)
+
+describe('Notifications', () => {
+  beforeEach((done) => { //Before each test we empty the database
+    Notification.remove({}, (err) => {
+      done()
+    })
+  })
+
+  describe('/POST a notification', () => {
+    it('it should CREATE and send a notification', (done) => {
+      let notification = getNotification('message', 'You have received a message.')
+      chai.request(server)
+        .post('/notifications/send')
+        .send(notification)
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.a('object')
+          res.body.information.should.to.be.eql('Notification has been sent')
+          res.body.notification.should.have.property('user')
+          res.body.notification.should.have.property('type').eql('message')
+          res.body.notification.should.have.property('url')
+          res.body.notification.should.have.property('text').eql('You have received a message.')
+          res.body.notification.should.have.property('visited').eql(false)
+          done()
+        })
+    })
+  })
+
+  describe('/POST a bad notification', () => {
+    it('it should NOT CREATE and send a notification', (done) => {
+      let notification = getNotification('', '')
+      chai.request(server)
+        .post('/notifications/send')
+        .send(notification)
+        .end((err, res) => {
+          res.should.have.status(500)
+          res.body.should.have.property('information').eql('Error when sending the notification to the database')
+          done()
+        })
+    })
+  })
+
+  describe('/GET/user/:idUser notifications', () => {
+    it('it should GET notifications by the given idUser', (done) => {
+      let userId = "5a78800f2b225224f01d167a"
+      let n1 = getNotification('message', 'You have received a message.')
+      let n2 = getNotification('like', 'You have received a like.')
+      let n3 = getNotification('welcome', 'Welcome to MeetYourLingo.')
+      n1.save((err, n1) => {
+        n2.save((err, n2) => {
+          n3.save((err, n3) => {
+            chai.request(server)
+              .get('/notifications/user/' + userId)
+              .end((err, res) => {
+                res.should.have.status(200)
+                res.body.should.be.a('array')
+                res.body.should.have.length(3)
+                done()
+              })
+          })
+        })
+      })
+    })
+  })
+
+  describe('/PUT/:id notification', () => {
+    it('it should UPDATE a notification given the id', (done) => {
+      let notification = getNotification('message', 'You have received a message.')
+      notification.save((err, notification) => {
+        chai.request(server)
+          .put('/notifications/' + notification.id)
+          .send({
+            visited: true
+          })
+          .end((err, res) => {
+            res.should.have.status(200)
+            res.body.should.be.a('object')
+            res.body.should.have.property('information').eql('Notification has been updated')
+            res.body.notification.should.have.property('visited').eql(true)
+            done()
+          })
+      })
+    })
+  })
+
+  describe('/PUT/:id notification (NOT FOUND)', () => {
+    it('it should NOT UPDATE a notification given the id', (done) => {
+      chai.request(server)
+        .put('/notifications/5a7dd492e6571a3e68d430f7')
+        .send({
+          visited: true
+        })
+        .end((err, res) => {
+          res.should.have.status(404)
+          res.body.should.be.a('object')
+          res.body.should.have.property('information').eql("Notification not found")
+          done()
+        })
+    })
+  })
+
+  // TODO
+  describe('/DELETE/:id notification', () => {
+    it('it should DELETE a notification given the id', (done) => {
+      let notification = getNotification('message', 'You have received a message.')
+      notification.save((err, notification) => {
+        chai.request(server)
+          .delete('/notifications/' + notification.id)
+          .end((err, res) => {
+            res.should.have.status(200)
+            res.body.should.be.a('object')
+            res.body.should.have.property('information').eql('Notification has been deleted')
+            done()
+          })
+      })
+    })
+  })
+
+  describe('/DELETE/:id notification NOT FOUND', () => {
+    it('it should fail at DELETE a notification given the id', (done) => {
+      chai.request(server)
+        .delete('/notifications/' + '5a7dd492e6571a3e68d430f7')
+        .end((err, res) => {
+          res.should.have.status(404)
+          res.body.should.be.a('object')
+          res.body.should.have.property('information').eql("Notification not found")
+          done()
+        })
+    })
+  })
+
+})
